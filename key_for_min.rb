28230ff168f72d6c8e467e8dc4f478430b0b6633
Heloise Taillet <ht2394@columbia.edu>
2019-01-03 23:43:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  number = 1000000000
+  name_hash.each do |key, value| 
+    if value <= number
+      number = value
+    end
+    return number 
+    
 
 end
