f57e7720c6b6bbedb9a67a29f24f4e2d8e4ddae9
Heloise Taillet <ht2394@columbia.edu>
2019-01-03 23:45:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,16 @@
 def key_for_min_value(name_hash)
   number = 1000000000
   name = ""
+  if name_hash.empty == true 
+    return nil
+  else
   name_hash.each do |key, value|
     if value <= number
       number = value
       name = key
     end
   end
+end
     return name
 
 
